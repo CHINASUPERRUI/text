6187e630c229feecdcc541d447efd0422107a4ae
RUI <superruier@foxmail.com>
2017-09-05 11:08:05 +0800

    TestUpdate
    测试更新

--- java1.java
+++ java1.java
@@ -2,7 +2,15 @@ public class Test{
 
   public static void main(String [args]){
 
-    System.out.println("Hello gungun!");
+    //Oabc o = new Oabc();
+    Oabc(abc);
+    System.out.println("Hello gungun!" + abc);
 
+  }
+  
+  private String Oabc(String abc){
+   
+    System.out.println("I Love You China..");
+    
   }
 }
